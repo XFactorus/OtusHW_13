915c369a1d002be4c00b1e4024ad95724c7f00f6
Vladyslav Pokryshka <vladyslav.pokryshka@quantag-it.com>
2020-11-01 17:41:40 +0200

    shared extension added

---
+++ OtusHW13/ShareText/ShareViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ShareViewController.swift
+//  ShareText
+//
+//  Created by Vladyslav Pokryshka on 31.10.2020.
+//
+
+import UIKit
+import Social
+
+class ShareViewController: SLComposeServiceViewController {
+
+    override func isContentValid() -> Bool {
+        return true
+    }
+    
+    override func didSelectPost() {
+        guard let text = textView.text else { return }
+        
+        guard let sharedDefaults = UserDefaults(suiteName: "group.otusvp.shared") else { return }
+        sharedDefaults.set(text, forKey: "sharedText")
+        
+        let u = URL(string: "OtusVPHW13://text")
+        
+        if let url = u {
+            _ = openURL(url)
+        }
+        
+        dismiss(animated: false) {
+            self.extensionContext!.completeRequest(returningItems: [], completionHandler: nil)
+        }
+    }
+    
+    @objc func openURL(_ url: URL) -> Bool {
+        var responder: UIResponder? = self
+        while responder != nil {
+            if let app = responder as? UIApplication {
+                return app.perform(#selector(openURL(_:)), with: url) != nil
+            }
+            responder = responder?.next
+        }
+        return false
+    }
+}
